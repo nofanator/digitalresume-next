4da501d88f4db3f9f9cc3fd01348b986a2182040
Chris Knopf <chris@nofmonster.com>
2021-07-21 13:59:46 -0500

    adding ga tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,31 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import ReactGA from 'react-ga'
+
 function MyApp({ Component, pageProps }) {
+    const router = useRouter()
+    useEffect(() => {
+        if (typeof window === undefined) return
+
+        const handleRouteChange = (url) => {
+            ReactGA.pageview(url)
+        }
+
+        if (!window.GA_INITIALIZED) {
+            ReactGA.initialize(`${process.env.NEXT_PUBLIC_GATRACKINGID}`)
+            window.GA_INITIALIZED = true
+        }
+
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        return () => {
+            if (typeof window === undefined) return
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
+    useEffect(() => {}, [])
+
     return <Component {...pageProps} />
 }
 
